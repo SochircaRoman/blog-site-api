8446fe48c0bf207c2bbeb325a841e71027b705f0
User01 <romansochirca30@gmail.com>
2023-01-19 13:12:08 +0200

    Add createUser method

--- server/src/database/repositories/UsersRepository.js
+++ server/src/database/repositories/UsersRepository.js
@@ -2,6 +2,24 @@ const User = require('../models/User')
 
 class UsersRepository {
   
+
+  async createUser(username, password, email, activationLink) {
+      
+      // Create a new user
+      const newUser = await User.create({ 
+          username: username,
+          password: password,
+          email: email,
+          activationLink: activationLink
+      })
+
+      // Check if new user was created
+      if (newUser) {
+          return newUser
+      }
+      return null;
+  }
+
 }
 
 module.exports = new UsersRepository()
