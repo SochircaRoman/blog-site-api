507d7369e27cf714ea28ddd83ca38db44ba58fb0
User01 <romansochirca30@gmail.com>
2023-01-19 13:22:39 +0200

    Add get methods

--- server/src/database/repositories/UsersRepository.js
+++ server/src/database/repositories/UsersRepository.js
@@ -1,7 +1,37 @@
 const User = require('../models/User')
 
 class UsersRepository {
-  
+  async getAllUsers(fields = undefined) {
+      return await User.findAll({attributes: fields})
+  }
+
+  async getUserById(id) {
+      return await User.findByPk(id)
+  }
+
+  async getUserByEmail(email) {
+      const existingUser = await User.findOne({ where: { email: email } })
+      if (existingUser) {
+          return existingUser
+      }
+      return null;
+  }
+
+  async getUserByUsername(username) {
+      const existingUser = await User.findOne({ where: { username: username } })
+      if (existingUser) {
+          return existingUser
+      }
+      return null;
+  }
+
+  async getUserByActivationLink(activationLink) {
+      const existingUser = await User.findOne({ where: { activationLink: activationLink } })
+      if (existingUser) {
+          return existingUser
+      }
+      return null;
+  }
 
   async createUser(username, password, email, activationLink) {
       
@@ -19,7 +49,6 @@ class UsersRepository {
       }
       return null;
   }
-
 }
 
 module.exports = new UsersRepository()
