7c8e0c22687bc905cb4161f045a75ab47b10d8b3
User01 <romansochirca30@gmail.com>
2023-01-19 19:06:24 +0200

    Add compPasswords method

--- server/src/database/repositories/AuthRepository.js
+++ server/src/database/repositories/AuthRepository.js
@@ -2,8 +2,11 @@ const bcrypt = require("bcryptjs")
 
 
 class AuthRepository {
-
-    
+  
+    async comparePasswords(password, hashPassword) {
+        // Compare and return the user password with hashed password from database
+        return await bcrypt.compare(password, hashPassword)
+    }
 
 }
 
