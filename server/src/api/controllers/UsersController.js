c1e0378a075bd805255c934d73eb64413f84fc8c
User01 <romansochirca30@gmail.com>
2023-01-19 19:35:10 +0200

    Add UsersController update methods

--- server/src/api/controllers/UsersController.js
+++ server/src/api/controllers/UsersController.js
@@ -2,7 +2,49 @@ const UsersService = require('../services/UsersService');
 
 class UsersController{
 
-    
+
+
+    async updateUsername(request, response){
+        try{
+            // Get and verify if request is not empty
+            const { username } = request.body
+            if (username == "") {
+                return response.status(404).json({message: "Nothing to update"});
+            }
+
+            // Verify if id is valid and user exist
+            const user = await UsersService.getOne(request.params.id)
+            if(!user){
+               return response.status(404).json({ error: "User with that id no exist" })
+            }
+
+            // Update user information
+            return response.status(200).json(await UsersService.updateUsername(username));
+        } catch(error){
+            return response.status(500).json(JSON.stringify(error))
+        }
+    }
+
+    async updateUserPassword(request, response){
+        try{
+            // Get and verify if request is not empty
+            const { oldPassword, newPassword} = request.body
+            if (!oldPassword && !newPassword) {
+                return response.status(404).json({message: "Nothing to update"});
+            }
+
+            // Verify if id is valid and user exist
+            const user = await UsersService.getOne(request.params.id)
+            if(!user){
+              return response.status(404).json({ error: "User with that id no exist" })
+            }
+
+            // Update user information
+            return response.status(200).json(await UsersService.updateUserPassword(oldPassword, newPassword));
+        } catch(error){
+            return response.status(500).json(JSON.stringify(error))
+        }
+    }
 
 }
 
