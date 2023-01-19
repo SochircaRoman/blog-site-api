f753caa977d67b003a04fd55b7d49257a638ccc0
User01 <romansochirca30@gmail.com>
2023-01-19 19:41:02 +0200

    Fix UsersController

--- server/src/api/controllers/UsersController.js
+++ server/src/api/controllers/UsersController.js
@@ -2,24 +2,22 @@ const UsersService = require('../services/UsersService');
 
 class UsersController{
 
-
-
     async updateUsername(request, response){
         try{
             // Get and verify if request is not empty
-            const { username } = request.body
-            if (username == "") {
+            const { newUsername } = request.body
+            if (newUsername == "") {
                 return response.status(404).json({message: "Nothing to update"});
             }
 
             // Verify if id is valid and user exist
-            const user = await UsersService.getOne(request.params.id)
+            const user = await UsersService.getUserById(request.params.id)
             if(!user){
                return response.status(404).json({ error: "User with that id no exist" })
             }
 
             // Update user information
-            return response.status(200).json(await UsersService.updateUsername(username));
+            return response.status(200).json(await UsersService.updateUsername(newUsername));
         } catch(error){
             return response.status(500).json(JSON.stringify(error))
         }
@@ -34,7 +32,7 @@ class UsersController{
             }
 
             // Verify if id is valid and user exist
-            const user = await UsersService.getOne(request.params.id)
+            const user = await UsersService.getUserById(request.params.id)
             if(!user){
               return response.status(404).json({ error: "User with that id no exist" })
             }
