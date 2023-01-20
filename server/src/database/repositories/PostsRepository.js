1530f03f2d494061beadcd2e65143a0755dce183
User01 <romansochirca30@gmail.com>
2023-01-20 18:05:03 +0200

    Add deleteAllPosts repository method

--- server/src/database/repositories/PostsRepository.js
+++ server/src/database/repositories/PostsRepository.js
@@ -1,7 +1,14 @@
 const Post = require('../models/Post')
 
 class PostsRepository {
-    
+
+    async deleteAllPosts(id) {
+        const deletedPosts = await Post.deleteMany({ id: id })
+        if (deletedPosts) {
+            return deletedPosts;
+        }
+        return null;
+    }
 }
 
 module.exports = new PostsRepository()
