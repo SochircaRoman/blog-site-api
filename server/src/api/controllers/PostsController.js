73136e2c8d2792b868aea9d53d49d266e0018e08
User01 <romansochirca30@gmail.com>
2023-01-21 18:04:02 +0200

    Add PostsController createPost method

--- server/src/api/controllers/PostsController.js
+++ server/src/api/controllers/PostsController.js
@@ -2,7 +2,21 @@ const PostsService = require('../services/PostsService')
 
 class PostsController{
 
-    
+    async createPost(request, response){
+        try{
+            // Create post
+            const createdPost = await PostsService.create(request.body)
+            if (!createdPost) {
+                return response.status(404).json({ error: "Post has not created" })
+            }
+
+            // Return created post
+            return response.status(200).json({ message: "Post has been created", post: createdPost });
+        } catch(error){
+            // Send error response
+            return response.status(500).json(JSON.stringify(error.message))
+        }
+    }
 }
 
 module.exports = new PostsController()
