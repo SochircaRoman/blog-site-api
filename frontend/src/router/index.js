2ca6878516a4c6954bbef3bbb3665515331f5bc3
User01 <romansochirca30@gmail.com>
2023-01-25 19:07:15 +0200

    Create router

---
+++ frontend/src/router/index.js
@@ -0,0 +1,18 @@
+import { createWebHistory, createRouter } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: HomeView
+    }
+  ]
+})
+
+export default router
