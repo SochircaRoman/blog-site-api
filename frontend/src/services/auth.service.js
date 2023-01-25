105bf780b5bf47a8844d3c25676adf4aedc465d3
User01 <romansochirca30@gmail.com>
2023-01-25 20:00:57 +0200

    Add register method

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,6 +1,21 @@
 import GenericService from './generic.service';
 
 class AuthService {
+
+  async register(user) {
+    const candidate = await GenericService.request({
+      url: 'auth/register',
+      method: 'post',
+      data: {
+        username: user.username,
+        password: user.password,
+        email: user.email
+      }
+    })
+
+    // If all ok return
+    return candidate;
+  }
   
 }
 
