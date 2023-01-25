2f5c08c86d65022dd2bfb5476a2e13fbf8b3b99b
User01 <romansochirca30@gmail.com>
2023-01-25 20:07:48 +0200

    Add login method

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -11,10 +11,31 @@ class AuthService {
         password: user.password,
         email: user.email
       }
-    })
+    });
 
     // If all ok return
     return candidate;
+  }
+
+  async login(user) {
+
+    // Try to login
+    const candidate = await GenericService.request({
+      url: 'auth/login',
+      method: 'post',
+      data: {
+        username: user.username,
+        password: user.password
+      }
+    });
+
+    // Add data to localStorage
+    if (candidate.data.accessToken) {
+      localStorage.setItem('user', JSON.stringify(candidate.data));
+    }
+
+    // If all ok return user data
+    return candidate.data;
   }
   
 }
