2f5f1e21fe5d481c4cce92d19976783c3273a952
User01 <romansochirca30@gmail.com>
2023-01-26 14:58:23 +0200

    Add get methods to userService

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -2,7 +2,35 @@ import GenericService from './generic.service';
 import authHeader from './auth-header';
 
 class UserService {
-  
+  getPublicContent() {
+    const content = GenericService.request({
+      url: 'users',
+      method: 'get',
+      data: {},
+      headers: authHeader()
+    });
+    return content;
+  }
+
+  getUserBoard() {
+    const content = GenericService.request({
+      url: `user/${id}`,
+      method: 'get',
+      data: {},
+      headers: authHeader()
+    });
+    return content;
+  }
+
+  getAdminBoard() {
+    const content = GenericService.request({
+      url: `user/${id}`,
+      method: 'get',
+      data: {},
+      headers: authHeader()
+    });
+    return content;
+  }
 }
 
 export default new UserService();
