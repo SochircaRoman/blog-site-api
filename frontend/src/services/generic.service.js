b4570f257371f7d1c50ec64c11e6caff51f5231a
User01 <romansochirca30@gmail.com>
2023-01-26 14:43:59 +0200

    Fix genericService

--- frontend/src/services/generic.service.js
+++ frontend/src/services/generic.service.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const API_URL = 'http://localhost:3000/api/'
 
 class GenericService {
-  async request({ url, method, data = {} }) {
-    const response = await axios[method](`${API_URL}/${url}`, data);
+  async request({ url, method, data = {}, headers = {} }) {
+    const response = await axios[method](`${API_URL}/${url}`, data, headers);
     return response;
   }
 }
