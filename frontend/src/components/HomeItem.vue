79263e8761fd22ba98de131a8ad767cc4afafe15
User01 <romansochirca30@gmail.com>
2023-02-09 19:20:48 +0200

    Fix accesToken

--- frontend/src/components/HomeItem.vue
+++ frontend/src/components/HomeItem.vue
@@ -5,7 +5,15 @@
 </template>
 
 <script>
+import UserService from "../services/user.service";
+
 export default {
+  name: "Home",
+  data() {
+    return {
+      content: "",
+    }
+  }
   
 }
 </script>
